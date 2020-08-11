ef01598053f02c9c045081604afe3bab03fd1755
Factis <53155715qwerty>
2020-08-11 15:40:44 +0800

    added MovieDetailPresenter

---
+++ app/src/main/java/com/foxy/movies/mvp/presenter/MovieDetailPresenter.kt
@@ -0,0 +1,9 @@
+package com.foxy.movies.mvp.presenter
+
+import com.foxy.movies.mvp.view.MovieDetailView
+import moxy.InjectViewState
+import moxy.MvpPresenter
+
+@InjectViewState
+class MovieDetailPresenter : MvpPresenter<MovieDetailView>() {
+}
