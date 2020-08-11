bd077cb0c07c0dc1fe2ce4a853223b9b2973fe8d
Factis <53155715qwerty>
2020-08-11 20:56:44 +0800

    changed to GenreWrapper

--- app/src/main/java/com/foxy/movies/mvp/view/MoviesListView.kt
+++ app/src/main/java/com/foxy/movies/mvp/view/MoviesListView.kt
@@ -1,5 +1,6 @@
 package com.foxy.movies.mvp.view
 
+import com.foxy.movies.data.GenreWrapper
 import com.foxy.movies.data.Movie
 import moxy.MvpView
 import moxy.viewstate.strategy.AddToEndSingleStrategy
@@ -8,11 +9,11 @@ import moxy.viewstate.strategy.StateStrategyType
 @StateStrategyType(value = AddToEndSingleStrategy::class)
 interface MoviesListView : MvpView {
 
-    fun onMoviesLoaded(genres: List<String>, movies: List<Movie>)
+    fun onMoviesLoaded(genres: List<GenreWrapper>, movies: List<Movie>)
 
     fun onMoviesNotAvailable(emptyText: Int)
 
-    fun updateView(genres: List<String>, movies: List<Movie>)
+    fun updateView(genres: List<GenreWrapper>, movies: List<Movie>)
 
     fun openMovieDetails(id: Int)
 
